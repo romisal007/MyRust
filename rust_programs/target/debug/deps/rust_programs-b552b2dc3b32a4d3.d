71bd8a8945db5f433f9354caefef1fe33b3d5f77
romisal007 <romisal007@gmail.com>
2020-05-16 21:31:35 +0530

    print x

---
+++ rust_programs/target/debug/deps/rust_programs-b552b2dc3b32a4d3.d
@@ -0,0 +1,5 @@
+h:\Rust\MyRust\rust_programs\target\debug\deps\rust_programs-b552b2dc3b32a4d3.rmeta: src\main.rs
+
+h:\Rust\MyRust\rust_programs\target\debug\deps\rust_programs-b552b2dc3b32a4d3.d: src\main.rs
+
+src\main.rs:
