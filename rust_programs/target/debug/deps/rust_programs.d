8138b1e82c59cae85849190df9b64963cb51bf11
romisal007 <romisal007@gmail.com>
2020-05-16 20:58:52 +0530

    Rust Hello World Program

---
+++ rust_programs/target/debug/deps/rust_programs.d
@@ -0,0 +1,5 @@
+H:\Rust\MyRust\rust_programs\target\debug\deps\rust_programs.exe: src\main.rs
+
+H:\Rust\MyRust\rust_programs\target\debug\deps\rust_programs.d: src\main.rs
+
+src\main.rs:
---
+++ rust_programs/target/debug/rust_programs.d
@@ -0,0 +1,1 @@
+H:\Rust\MyRust\rust_programs\target\debug\rust_programs.exe: H:\Rust\MyRust\rust_programs\src\main.rs
